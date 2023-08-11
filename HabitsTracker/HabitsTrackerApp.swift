16afd6cff4397a639a68b5ee4b601e1ff2cca9aa
sebastian.popa <sebastian.popa@softvision.com>
2023-08-12 01:27:18 +0300

    Initial Commit

---
+++ HabitsTracker/HabitsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsTrackerApp.swift
+//  HabitsTracker
+//
+//  Created by sebastian.popa on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
